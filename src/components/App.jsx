67d6c96efbf10df1b675c41b8e9eff6df1e2ed19
Dadayat <136356333+Dadayat@users.noreply.github.com>
2023-12-12 21:27:43 +0200

    upd profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
 import { Profile } from "./Profile";
+import user from "./data/user.json";
 
 export const App = () => {
   return (
     <div>
-      <Profile />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats} />
     </div>
   );
 };
